b468ed9722fdef82075ed1fcf64c938520e0b29c
StevenJ0 <stevenjonathanalfredo785@gmail.com>
2025-05-06 00:03:06 +0700

    last modified

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 import "../styles/globals.css";
 
-function MyApp({
-  Component,
-  pageProps: { session, ...pageProps }
-}: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* Alternatif untuk modern browsers */}
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      
+      {mounted && <Component {...pageProps} />}
     </SessionProvider>
   );
 }
